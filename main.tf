1154c78022e76c39387aec048625d4a40df12b29
akshayreddy2196 <akshayreddy2196@gmail.com>
2025-09-16 14:56:40 +0530

    changes

--- main.tf
+++ main.tf
@@ -13,11 +13,11 @@ module "lambda" {
   source        = "./modules/lambda"
   function_name = "hello-lambda"
   image_uri     = "var.repository_name"
-  role_arn      = module.iam.aws_iam_role.this.arn
+  role_arn      = module.iam.role_arn
 }
 
 module "apigateway" {
   source     = "./modules/apigateway"
   api_name   = "hello-api"
-  lambda_arn = module.lambda.aws_lambda_function.this.arn
+  lambda_arn = module.lambda.lambda_arn
 }
